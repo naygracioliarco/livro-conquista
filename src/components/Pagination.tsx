999946e53ea63449f8805095e4f6d7d5eee4c331
nayaragracioliarco <nayaragracioli@arcoeducacao.com>
2025-11-28 18:49:47 -0300

    paginação

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,4 +1,3 @@
-import { ChevronDown } from 'lucide-react';
 
 interface PaginationProps {
   currentPage: number;
@@ -7,19 +6,30 @@ interface PaginationProps {
 function Pagination({ currentPage }: PaginationProps) {
   return (
     <div
-      className="flex items-center justify-center"
+      className="flex items-center justify-center -mx-8 md:-mx-12"
       style={{
         display: 'flex',
-        width: '100%',
+        width: '100vw',
+        maxWidth: '100vw',
         padding: '4px 360px',
         justifyContent: 'center',
         alignItems: 'center',
         gap: '10px',
         backgroundColor: '#F8B9CB',
+        marginBottom: '40px',
+        position: 'relative',
+        left: '50%',
+        right: '50%',
+        marginLeft: '-50vw',
+        marginRight: '-50vw',
       }}
     >
       <span>Página - {currentPage}</span>
-      <ChevronDown size={20} />
+      <img
+                  src="/images/seta.svg"
+                  alt="Union"
+                  className="w-3 h-3 object-contain"
+                />
     </div>
   );
 }
